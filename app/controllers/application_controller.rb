471545ffcf4321c23603bb98f67a7d3fae551891
Egon Fiedler <efiedler93@gmail.com>
2018-11-02 21:41:24 -0700

    adding first route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true , class ApplicationController < ActionController::Base
 class ApplicationController < ActionController::Base
   protect_from_forgery with :exception
 
   def hello
     render html: 'hello, world!'
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
   end
 end
